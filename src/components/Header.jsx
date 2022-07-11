df3633730e1757d49e389e681f0db2dba932d198
Zubair <zubairkamboh9010@gmail.com>
2022-07-11 11:33:44 +0500

    Added services dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
 import '../assets/styles/header.css'
 import HeaderButton from './HeaderButton'
 import logo from '../assets/images/site-logo.png'
@@ -18,9 +18,50 @@ const Header = () => {
             <NavLink className="navlink" to="/">
               Home
             </NavLink>
-            <NavLink className="navlink" to="/">
-              Services
+            <NavLink className="navlink" to="/fare-page">
+              Fare
             </NavLink>
+            <NavDropdown
+              title="Services"
+              style={{ color: '#000000 !important' }}
+              id="nav-dropdown"
+              className="navlink"
+            >
+              <NavDropdown.Item eventKey="4.1" href="/airport-transport">
+                Airport Transport
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.1" href="/private-car">
+                Private Car
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.1" href="/point-to-point">
+                Point to Point
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.1" href="/corporate-transportation">
+                Corporate Transportation
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.1" href="/limo-service">
+                Limo Service
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.1" href="/party-limosine">
+                Party Service
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* <NavLink className="navlink" to="/private-car">
+              Private Car
+            </NavLink>
+            <NavLink className="navlink" to="/point-to-point">
+              Point to Point
+            </NavLink>
+            <NavLink className="navlink" to="/corporate-transportation">
+              Corporate
+            </NavLink>
+            <NavLink className="navlink" to="/limo-service">
+              Limo
+            </NavLink>
+            <NavLink className="navlink" to="/party-limosine">
+              Party
+            </NavLink> */}
             <NavLink className="navlink" to="/about-us">
               About
             </NavLink>
